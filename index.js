6de96619fbae31c4df822e574fa2c3afaf81c95b
shimpy-gw <42718222+shimpy-gw@users.noreply.github.com>
2018-10-31 19:40:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ bot.on("message", function(message) {
                 .addField("Temps de latence", "`ping` - Votre ping entre vous et la conversation discord")
                 .addField("Liens utiles", "[Lien de téléchargement de Quidol](https://play.google.com/store/apps/details?id=com.quidol.app.prod&hl=en) \n [Site internet de Quidol](http://www.quidol.fr/)")
                 .setColor("#E30000")
-                .setFooter("QWT - TRIVIA")
+                .setFooter("QWCT - TRIVIA")
                 .setThumbnail("https://lh3.googleusercontent.com/WEVLYerVfnoz4L6gtuE6H2euZmlN-J7FC4vVHQs6kQg5gzvf2Z0AtSz0HDnOf1qfYlc=s180")
                 .setTimestamp()
             message.channel.sendEmbed(embed);
@@ -43,8 +43,20 @@ bot.on("message", function(message) {
             .addField("Temps de latence", "`ping` - Votre ping entre vous et la conversation discord")
             .addField("Liens utiles", "[Lien de téléchargement de Wizzup](https://play.google.com/store/apps/details?id=com.wizzup.app.prod) \n [Site internet de Wizzup](http://www.wizzupapp.fr/)")
             .setColor("#006AE3")
-            .setFooter("QWT - TRIVIA")
+            .setFooter("QWCT - TRIVIA")
             .setThumbnail("https://lh3.googleusercontent.com/vtnbSMVEo-rb_raoCm8J6VCKGWqFrmjCnMeP6aO1-IuHL7eJ7Cjx9P9eqnABxF5zEw=s180-rw")
+            .setTimestamp()
+        message.channel.sendEmbed(embed);
+        break;
+      case "thegame":
+            var embed = new Discord.RichEmbed()
+            .setTitle("The Game va commencer dans quelques minutes..")
+            .setDescription("Preparez-vous, le jeu The Game va commencer dans quelques minutes. Assurez-vous d'avoir une bonne connexion internet !")
+            .addField("Temps de latence", "`ping` - Votre ping entre vous et la conversation discord")
+            .addField("Liens utiles", "[Lien de téléchargement de The Game](https://play.google.com/store/apps/details?id=com.thegame.app.prod) \n [L'adresse mail de The Game](admin.mobile@webedia-group.com)")
+            .setColor("#2200FE")
+            .setFooter("QWCT - TRIVIA")
+            .setThumbnail("https://lh3.googleusercontent.com/Brjn5pZhva2N81Pjg1K677ZOxsTUelqgifG812f7qR3TXzN74u29agFYlPpdZbbsCrM=s180-rw")
             .setTimestamp()
         message.channel.sendEmbed(embed);
         break;
